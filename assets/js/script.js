bd6c88b9a01708380f19acfd556991462f9af57d
Makaosva <maka.osses@gmail.com>
2024-04-08 22:05:15 -0400

    feat/Agregar htmls y arreglo de funciones

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+/*function pintar() {
+  ele.style.backgroundColor = "yellow";
+}*/
+const ele = document.getElementById("ele1");
+//ele.addEventListener("click", pintar);
+ele.addEventListener("click", function () {
+  ele.style.backgroundColor = "yellow";
+});
