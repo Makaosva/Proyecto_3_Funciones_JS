153405cae983211ab4ea0031890c4d2397733e2c
Makaosva <maka.osses@gmail.com>
2024-04-12 21:16:32 -0400

    feat/Arrgler html pintar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
+/*----------PARTE 2.1----------*/
 /*function pintar() {
   ele.style.backgroundColor = "yellow";
 }*/
 const ele = document.getElementById("ele1");
 //ele.addEventListener("click", pintar);
-ele.addEventListener("click", function (color = "green") {
+ele.addEventListener("click", function () {
+  color = "green";
   console.log("color");
   ele.style.backgroundColor = color;
 });
